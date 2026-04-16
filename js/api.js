95feaef4667854a920ca7212a655f0d58b719279
Yasmin Zancan <yazancan@gmail.com>
2026-04-16 17:34:47 -0300

    Troca fetch por axios

--- js/api.js
+++ js/api.js
@@ -3,8 +3,8 @@ const URL_BASE = "http://localhost:3000"
 const api = {
   async buscarPensamentos() {
     try {
-      const response = await fetch(`${URL_BASE}/pensamentos`);
-      return await response.json();
+      const response = await axios.get(`${URL_BASE}/pensamentos`);
+      return await response.data;
     } 
     catch {
       alert("Erro ao buscar pensamentos");
@@ -14,14 +14,8 @@ const api = {
 
   async salvarPensamento(pensamento) {
     try {
-      const response = await fetch(`${URL_BASE}/pensamentos`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(pensamento)
-      });
-      return await response.json();
+      const response = await axios.post(`${URL_BASE}/pensamentos`, pensamento);
+      return await response.data;
     } 
     catch {
       alert("Erro ao salvar pensamentos");
@@ -31,8 +25,8 @@ const api = {
 
   async buscarPensamentoPorId(id) {
     try {
-      const response = await fetch(`${URL_BASE}/pensamentos/${id}`);
-      return await response.json();
+      const response = await axios.get(`${URL_BASE}/pensamentos/${id}`);
+      return await response.data;
     } 
     catch {
       alert("Erro ao buscar pensamento");
@@ -42,14 +36,8 @@ const api = {
 
   async editarPensamento(pensamento) {
     try {
-      const response = await fetch(`${URL_BASE}/pensamentos/${pensamento.id}`, {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(pensamento)
-      });
-      return await response.json();
+      const response = await axios.put(`${URL_BASE}/pensamentos/${pensamento.id}`, pensamento);
+      return await response.data;
     } 
     catch {
       alert("Erro ao editar pensamento");
@@ -59,9 +47,7 @@ const api = {
 
   async excluirPensamento(id) {
     try {
-      const response = await fetch(`${URL_BASE}/pensamentos/${id}`, {
-        method: "DELETE",
-      });
+      const response = await axios.delete(`${URL_BASE}/pensamentos/${id}`,);
     } 
     catch {
       alert("Erro ao excluir um pensamento");
